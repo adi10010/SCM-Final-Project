0698d364be85f14d3e96924b1f8e9366945a8f16
adwit001 <157448523+adwit001@users.noreply.github.com>
2024-05-16 23:23:57 +0530

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -105,6 +105,16 @@ $(document).ready(function() {
         for (var i = 0; i < newIngredient.length; i++){
          $(this).parent().siblings("ul").append("<li>" + newCheckbox + newIngredient[i] + "  (+Rs.3)</li>");
         }
+                
+                $('#recipe1').children('li').click(checkboxChange(1));
+                $('#recipe2').children('li').click(checkboxChange(2));
+                $('#recipe3').children('li').click(checkboxChange(3));
+                $('#recipe3').children('li').click(checkboxChange(4));
+                $('#recipe3').children('li').click(checkboxChange(5));
+                $('#recipe3').children('li').click(checkboxChange(6));
+
+                $(this).parent().remove();
+
 
 
 
