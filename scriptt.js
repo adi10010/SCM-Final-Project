db0aa7ad4ae826cdfccd6c4f8027812c9a504afc
adwit001 <157448523+adwit001@users.noreply.github.com>
2024-05-16 22:35:04 +0530

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -4,3 +4,10 @@ $(document).ready(function() {
     $( "a" ).click(function( event ) {
           event.preventDefault();
     });
+    
+    var numOfOrders = 0;
+    $(".num").text(numOfOrders);
+
+    // hide dialogs on start
+    $("#thanksMessage, #checkOrderHamburger, #checkOrderMomo, #checkOrderPizza, #checkOrderFries, #checkOrderChowmien, #checkOrderbiryani, #finishOrderDialog").hide();
+
